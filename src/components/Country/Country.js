18570a597f04f1b46dbd6222784025b6a0b38002
Tauhid <tauhid.swe@gmail.com>
2022-09-28 03:21:53 +0600

    added style in country

---
+++ src/components/Country/Country.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    return (
+        <div className="country">
+            <h1>{props.name}</h1>
+            <h4>Population: {props.population}</h4>
+            <p>Area: {props.area}</p>
+        </div>
+    );
+};
+
+export default Country;
